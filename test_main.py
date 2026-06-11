d83c4edd39de36be79c21340574015f11bc482e2
Vo Thi Hong Nhung <vthnhung1604@gmail.com>
2026-06-11 14:32:20 +0700

    chore: add unit test and github actions

--- test_main.py
+++ test_main.py
@@ -1,9 +1,13 @@
 import unittest
 from main import tinh_diem_gpa
+
 class TestGpaFunctions(unittest.TestCase):
-  def test_gpa_he_muoi(self):
-    self.assertEqual(tinh_diem_gpa(8.5), 4.0)
-  def test_gpa_tuyen_tinh(self):
-    self.assertEqual(tinh_diem_gpa(7.0), 2.8)
-  if__name__=='__main__':
+
+    def test_gpa_he_muoi(self):
+        self.assertEqual(tinh_diem_gpa(8.5), 4.0)
+
+    def test_gpa_tuyen_tinh(self):
+        self.assertEqual(tinh_diem_gpa(7.0), 2.8)
+
+if __name__ == '__main__':
     unittest.main()
