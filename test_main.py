35a3e727e63929c193f9581b3711ce5a7ae637d7
vthnhung1604-star <vthnhung1604@gmail.com>
2026-06-11 13:58:22 +0700

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,9 @@
+import unittest
+from main import tinh_diem_gpa
+class TestGpaFunctions(unittest.TestCase):
+  def test_gpa_he_muoi(self):
+    self.assertEqual(tinh_diem_gpa(8.5), 4.0)
+  def test_gpa_tuyen_tinh(self):
+    self.assertEqual(tinh_diem_gpa(7.0), 2.8)
+  if__name__=='__main__':
+    unittest.main()
