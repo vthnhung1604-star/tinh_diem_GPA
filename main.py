7171092771013f7bcd26715a25f18a29fcb452a3
Thanh Hien <thanhhien260105@gmail.com>
2026-06-11 14:55:13 +0700

    feat: Sửa lại đúng công thức tính gpa theo hệ số 4

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ def tinh_diem_gpa(diem_so):
     if diem_so >= 8.5:
         return 4.0
     else:
-        return (diem_so / 10) * 3.5
+        return round((diem_so / 10) * 4, 2)
 
 print("Diem GPA he 4 la:", tinh_diem_gpa(8.5))
